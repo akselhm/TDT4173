94baf54e29c7c86b1343819b81e0f2d5975934a1
akselmadslien <aksel.madslien@teknologiporten.no>
2020-11-02 11:12:58 +0100

    renamed ensemble.py to main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd 
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import BaggingClassifier, RandomForestClassifier
+from sklearn.ensemble import BaggingClassifier, RandomForestClassifier, AdaBoostClassifier
 from sklearn.tree import DecisionTreeClassifier
 
 #read dataset
@@ -12,7 +12,10 @@ array = dataset.values
 X = array[:,0:-1]
 Y = array[:,-1]
 
-x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.25, random_state=4)
+#print(X[:10])
+#print(Y[:10])
+
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.25, random_state=10)
 
 # Decition Tree (To see difference)
 
@@ -24,7 +27,7 @@ print("Decision Tree on test data",dTree.score(x_test, y_test))
 
 print("Decision Tree on training data",dTree.score(x_train, y_train))
 
-#Random Forest Classifier
+#Random Forest Classifier (Bagging)
 
 RandomForest = RandomForestClassifier(n_estimators = 30)
 RandomForest.fit(x_train,y_train)
@@ -32,4 +35,17 @@ RandomForest.fit(x_train,y_train)
 print("Random Forest on test data",RandomForest.score(x_test, y_test))
 
 print("Random Forest on training data",RandomForest.score(x_train, y_train))
+
+#Bagging (w/ decision trees)
+
+Bagging = BaggingClassifier(DecisionTreeClassifier(), max_samples= 0.5, max_features = 1.0, n_estimators = 30)
+#NOTE: bad results. needs tuning
+Bagging.fit(x_train,y_train)
+
+print("Bagging Forest on test data",Bagging.score(x_test, y_test))
+
+print("Bagging on training data",Bagging.score(x_train, y_train))
+
+# AdaBoost (Boosting)
+
 
